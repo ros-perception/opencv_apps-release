fac054d1ccee2b8fb984b758084e0fe0b995aef1
Tokyo Opensource Robotics Developer 534 <534o@opensource-robotics.tokyo.jp>
2016-09-12 12:56:31 +0000

    Rebase from 'release/indigo/opencv_apps'

--- src/nodelet/face_detection_nodelet.cpp
+++ src/nodelet/face_detection_nodelet.cpp
@@ -43,6 +43,7 @@
 #include "opencv_apps/nodelet.h"
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
 
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -64,8 +65,10 @@ class FaceDetectionNodelet : public opencv_apps::Nodelet
 
   boost::shared_ptr<image_transport::ImageTransport> it_;
 
-  face_detection::FaceDetectionConfig config_;
-  dynamic_reconfigure::Server<face_detection::FaceDetectionConfig> srv;
+  typedef face_detection::FaceDetectionConfig Config;
+  typedef dynamic_reconfigure::Server<Config> ReconfigureServer;
+  Config config_;
+  boost::shared_ptr<ReconfigureServer> reconfigure_server_;
 
   bool debug_view_;
   ros::Time prev_stamp_;
@@ -73,7 +76,7 @@ class FaceDetectionNodelet : public opencv_apps::Nodelet
   cv::CascadeClassifier face_cascade_;
   cv::CascadeClassifier eyes_cascade_;
 
-  void reconfigureCallback(face_detection::FaceDetectionConfig &new_config, uint32_t level)
+  void reconfigureCallback(Config &new_config, uint32_t level)
   {
     config_ = new_config;
   }
@@ -101,7 +104,7 @@ class FaceDetectionNodelet : public opencv_apps::Nodelet
     try
     {
       // Convert the image into something opencv can handle.
-      cv::Mat frame = cv_bridge::toCvShare(msg, msg->encoding)->image;
+      cv::Mat frame = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image;
 
       // Messages
       opencv_apps::FaceArrayStamped faces_msg;
@@ -207,9 +210,10 @@ public:
     }
     prev_stamp_ = ros::Time(0, 0);
 
-    dynamic_reconfigure::Server<face_detection::FaceDetectionConfig>::CallbackType f =
+    reconfigure_server_ = boost::make_shared<dynamic_reconfigure::Server<Config> >(*pnh_);
+    dynamic_reconfigure::Server<Config>::CallbackType f =
       boost::bind(&FaceDetectionNodelet::reconfigureCallback, this, _1, _2);
-    srv.setCallback(f);
+    reconfigure_server_->setCallback(f);
     
     img_pub_ = advertiseImage(*pnh_, "image", 1);
     msg_pub_ = advertise<opencv_apps::FaceArrayStamped>(*pnh_, "faces", 1);
